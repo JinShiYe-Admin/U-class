132d729acad0ad0c34c8fefec39657ba550167ed
Zqw <578159259@qq.com>
2017-08-17 10:14:00 +0800

    通过第三方程序调起，得到的token获取个人信息

---
+++ js/common/publicProtocol.js
@@ -0,0 +1,122 @@
+//签名
+document.write('<script src="../../js/libs/crypto-js/require.js"><\/script>');
+document.write('<script src="../../js/utils/signHmacSHA1.js"><\/script>');
+//本地存储
+document.write('<script src="../../js/libs/myStorage/myStorage.js"><\/script>');
+document.write('<script src="../../js/common/storageKeyName.js"><\/script>');
+document.write('<script src="../../js/libs/jquery.js"><\/script>');
+
+//url,
+//encryData,需要加密的字段
+//commonData,不需要加密的对象
+//flag,0表示不需要合并共用数据，1为添加uuid、utid、token、appid普通参数，2为uuid、appid、token
+//waitingDialog,等待框
+//callback,返回值
+function postDataEncry(url, encryData, commonData, flag, callback) {
+	if(plus.networkinfo.getCurrentType() == plus.networkinfo.CONNECTION_NONE) {
+		var data = {
+				RspCode: '404',
+				RspData: '',
+				RspTxt: '网络异常，请检查网络设置！'
+		}
+			callback(data);
+		return;
+	}
+	//拼接登录需要的签名
+	var signTemp = postDataEncry1(encryData, commonData, flag);
+
+	//生成签名，返回值sign则为签名
+	signHmacSHA1.sign(signTemp, storageKeyName.SIGNKEY, function(sign) {
+		//组装发送握手协议需要的data
+		//合并对象
+		var tempData = $.extend(encryData, commonData);
+		//添加签名
+		tempData.sign = sign;
+		// 等待的对话框
+		var urlArr = url.split('/');
+		console.log('传递的参数' + urlArr[urlArr.length - 1] + ':', tempData);
+		var tepTime = tempTime();
+		//发送协议
+		mui.ajax(url, {
+			data: JSON.stringify(tempData),
+			dataType: 'json',
+			type: 'post',
+			contentType: "application/json",
+			timeout: tepTime,
+			success: function(data) {
+				console.log(urlArr[urlArr.length - 1] + "接口获取的值:", data);
+				callback(data);
+			},
+			error: function(xhr, type, errorThrown) {
+				console.log("网络连接失败" + url + ":" + type + "," + errorThrown + ":", xhr);
+				var data = {
+					RspCode: '404',
+					RspData: '',
+					RspTxt: '网络连接失败，请重新尝试一下'
+				}
+				callback(data);
+			}
+		});
+	});
+}
+
+function tempTime() {
+	switch(plus.os.name) {
+		case "Android":
+			return 30000;
+			break;
+		case "iOS":
+			return 300000;
+			break;
+		default:
+			// 其它平台
+			break;
+	}
+}
+
+//拼接参数
+function postDataEncry1(encryData, commonData, flag) {
+	//循环
+	var tempStr = '';
+	for(var tempData in encryData) {
+		//对value进行加密
+		var encryptStr = RSAEncrypt.enctype(encryData[tempData]);
+		//修改值
+		encryData[tempData] = encryptStr;
+	}
+	//判断是否需要添加共用数据
+	if(flag == 1) {
+		//获取个人信息
+		
+	} else if(flag == 2) {
+		//获取个人信息
+		var personalToken = window.myStorage.getItem(window.storageKeyName.PERSONALINFO).token;
+		var comData = {
+			uuid: plus.device.uuid,
+			token: personalToken,
+			appid: plus.runtime.appid
+		};
+		commonData = $.extend(commonData, comData);
+	} else if(flag == 3) {
+		//获取个人信息
+		var personalToken = window.myStorage.getItem(window.storageKeyName.PERSONALINFO).token;
+		var comData = {
+			token: personalToken
+		};
+		commonData = $.extend(commonData, comData);
+	}
+	//将对象转为数组
+	var arr0 = [];
+	for(var item in encryData) {
+		arr0.push(item + '=' + encryData[item]);
+	};
+	var arr1 = [];
+	for(var item in commonData) {
+		arr1.push(item + '=' + commonData[item]);
+	};
+	//合并数组
+	var signArr = arr0.concat(arr1);
+	//拼接登录需要的签名
+	var signTemp = signArr.sort().join('&');
+	return signTemp;
+}
---
+++ js/common/storageKeyName.js
@@ -0,0 +1,125 @@
+//此js用于保存本地存储时，用到的key值
+
+var storageKeyName = (function(mod) {
+
+	mod.key = 0; //0,开发;1,测试--教宝云;2,测试--微视学堂 3外网--微视学堂 4外网--教宝云
+	var exLog = console.log;
+	console.log = function(hint, object) {
+		if(mod.key === 0) {
+			var argus = hint;
+			if(object) {
+				argus = hint + JSON.stringify(object);
+			}
+			exLog.apply(this, [argus]);
+		}
+	}
+	switch(mod.key) {
+		case 0: //开发
+			//---开发---start---
+			mod.MAINEDU = 'http://192.168.1.44:8511/'; //科教图片url
+			mod.MAINURL = 'http://192.168.1.44:8511/api/CloudApi/'; //主url
+			mod.MAINJIAOXIAOURL = 'http://192.168.1.200:8081/JiaoBaoCloudService/'; //家校圈url
+			mod.MAINHOMEWORKURL = 'http://192.168.1.44:8513/'; //作业主url
+			mod.MAINQIUZHI = 'http://192.168.1.200:8081/JiaoBaoCloudService/'; //求知主url
+			mod.MAINMICROCLASS = 'http://192.168.1.200:8081/JiaoBaoCloudService/'; //微课主url
+			//---开发---end---
+			//---七牛空间和接口---开发---start---
+			mod.QNPB = 'http://qn-kfpb.jiaobaowang.net/'; //公开空间域名
+			mod.QNGETUPLOADTOKEN = 'http://114.215.222.186:8004/Api/QiNiu/GetUpLoadToKen';
+			mod.QNGETUPTOKENHEADIMGE = 'http://114.215.222.186:8004/Api/QiNiu/GetUpLoadToKen'; //获取上传个人头像，群头像，资料头像到七牛的token的url
+			mod.QNGETUPTOKENFILE = 'http://114.215.222.186:8004/Api/QiNiu/GetUpLoadToKen'; //获取上传文件（云存储）到七牛的token的url
+			mod.QNGETDOWNTOKENFILE = 'http://114.215.222.186:8004/Api/QiNiu/GetAccess'; //获取下载文件（云存储）的token的url，url+七牛文件url
+			mod.QNGETTOKENDELETE = 'http://114.215.222.186:8004/Api/QiNiu/Delete'; //获取批量（或者一个）删除七牛文件的token的url
+			//	---七牛空间和接口---开发---end---
+			break;
+		case 1: //测试--教宝云
+			//---测试---start---
+			mod.MAINEDU = 'http://192.168.1.178:8511/'; //科教图片url
+			mod.MAINURL = 'http://192.168.1.178:8511/api/CloudApi/'; //主url
+			mod.MAINJIAOXIAOURL = 'http://192.168.1.178:8080/JiaoBaoCloudService/'; //家校圈url
+			mod.MAINHOMEWORKURL = 'http://192.168.1.178:8088/'; //作业主url
+			mod.MAINQIUZHI = 'http://192.168.1.178:8080/JiaoBaoCloudService/'; //求知主url
+			mod.MAINMICROCLASS = 'http://192.168.1.178:8080//JiaoBaoCloudService/'; //微课主url
+			//---测试---end---
+
+			//---七牛空间和接口---测试---start---
+			mod.QNPB = 'http://qn-cspb.jiaobaowang.net/'; //公开空间域名
+			mod.QNGETUPLOADTOKEN = 'http://114.215.222.186:8005/Api/QiNiu/GetUpLoadToKen';
+			mod.QNGETUPTOKENHEADIMGE = 'http://114.215.222.186:8005/Api/QiNiu/GetUpLoadToKen'; //获取上传个人头像，群头像，资料头像到七牛的token的url
+			mod.QNGETUPTOKENFILE = 'http://114.215.222.186:8005/Api/QiNiu/GetUpLoadToKen'; //获取上传文件（云存储）到七牛的token的url
+			mod.QNGETDOWNTOKENFILE = 'http://114.215.222.186:8005/Api/QiNiu/GetAccess'; //获取下载文件（云存储）的token的url，url+七牛文件url
+			mod.QNGETTOKENDELETE = 'http://114.215.222.186:8005/Api/QiNiu/Delete'; //获取批量（或者一个）删除七牛文件的token的url
+			//---七牛空间和接口---测试---end---
+			break;
+			
+		case 2: //测试--微视学堂
+			//---移动版---start---
+			mod.MAINEDU = 'http://192.168.1.178:8020/'; //科教图片url
+			mod.MAINURL = 'http://192.168.1.178:8020/api/CloudApi/'; //主url
+			mod.MAINJIAOXIAOURL = 'http://192.168.1.178:8080/JiaoBaoCloudService/'; //家校圈url
+			mod.MAINHOMEWORKURL = 'http://192.168.1.178:8021/'; //作业主url
+			mod.MAINQIUZHI = 'http://192.168.1.178:8080/JiaoBaoCloudService/'; //求知主url
+			mod.MAINMICROCLASS = 'http://192.168.1.178:8080/JiaoBaoCloudService/'; //微课主url
+			//---内网---end---
+
+			//---七牛空间和接口---测试---start---
+			mod.QNPB = 'http://qn-cspb.jiaobaowang.net/'; //公开空间域名
+			mod.QNGETUPLOADTOKEN = 'http://114.215.222.186:8005/Api/QiNiu/GetUpLoadToKen';
+			mod.QNGETUPTOKENHEADIMGE = 'http://114.215.222.186:8005/Api/QiNiu/GetUpLoadToKen'; //获取上传个人头像，群头像，资料头像到七牛的token的url
+			mod.QNGETUPTOKENFILE = 'http://114.215.222.186:8005/Api/QiNiu/GetUpLoadToKen'; //获取上传文件（云存储）到七牛的token的url
+			mod.QNGETDOWNTOKENFILE = 'http://114.215.222.186:8005/Api/QiNiu/GetAccess'; //获取下载文件（云存储）的token的url，url+七牛文件url
+			mod.QNGETTOKENDELETE = 'http://114.215.222.186:8005/Api/QiNiu/Delete'; //获取批量（或者一个）删除七牛文件的token的url
+			break;
+
+		case 3: //外网--微视学堂
+			//---移动版---start---
+			mod.MAINEDU = 'http://114.215.222.186:8009/'; //科教图片url
+			mod.MAINURL = 'http://114.215.222.186:8009/api/CloudApi/'; //主url
+			mod.MAINJIAOXIAOURL = 'http://114.215.222.194:8088/JiaoBaoCloudService/'; //家校圈url
+			mod.MAINHOMEWORKURL = 'http://114.215.222.186:8008/'; //作业主url
+			mod.MAINQIUZHI = 'http://114.215.222.194:8088/JiaoBaoCloudService/'; //求知主url
+			mod.MAINMICROCLASS = 'http://114.215.222.194:8088/JiaoBaoCloudService/'; //微课主url
+			//---外网---end---
+
+			//---七牛空间和接口---测试---start---
+			mod.QNPB = 'http://qn-cspb.jiaobaowang.net/'; //公开空间域名
+			mod.QNGETUPLOADTOKEN = 'http://114.215.222.186:8005/Api/QiNiu/GetUpLoadToKen';
+			mod.QNGETUPTOKENHEADIMGE = 'http://114.215.222.186:8005/Api/QiNiu/GetUpLoadToKen'; //获取上传个人头像，群头像，资料头像到七牛的token的url
+			mod.QNGETUPTOKENFILE = 'http://114.215.222.186:8005/Api/QiNiu/GetUpLoadToKen'; //获取上传文件（云存储）到七牛的token的url
+			mod.QNGETDOWNTOKENFILE = 'http://114.215.222.186:8005/Api/QiNiu/GetAccess'; //获取下载文件（云存储）的token的url，url+七牛文件url
+			mod.QNGETTOKENDELETE = 'http://114.215.222.186:8005/Api/QiNiu/Delete'; //获取批量（或者一个）删除七牛文件的token的url
+			break;
+		case 4: //外网--教宝云
+			//---外网---start---
+			mod.MAINEDU = 'http://114.215.222.186:8002/'; //科教图片url
+			mod.MAINURL = 'http://114.215.222.186:8002/api/CloudApi/'; //主url
+			mod.MAINJIAOXIAOURL = 'http://114.215.222.194:8080/JiaoBaoCloudService/'; //家校圈url
+			mod.MAINHOMEWORKURL = 'http://114.215.222.186:8001/'; //作业主url
+			mod.MAINQIUZHI = 'http://114.215.222.194:8080/JiaoBaoCloudService/'; //求知主url
+			mod.MAINMICROCLASS = 'http://114.215.222.194:8080/JiaoBaoCloudService/'; //微课主url
+			//---外网---end---
+
+			//---七牛空间和接口---测试---start---
+			mod.QNPB = 'http://qn-cspb.jiaobaowang.net/'; //公开空间域名
+			mod.QNGETUPLOADTOKEN = 'http://114.215.222.186:8005/Api/QiNiu/GetUpLoadToKen';
+			mod.QNGETUPTOKENHEADIMGE = 'http://114.215.222.186:8005/Api/QiNiu/GetUpLoadToKen'; //获取上传个人头像，群头像，资料头像到七牛的token的url
+			mod.QNGETUPTOKENFILE = 'http://114.215.222.186:8005/Api/QiNiu/GetUpLoadToKen'; //获取上传文件（云存储）到七牛的token的url
+			mod.QNGETDOWNTOKENFILE = 'http://114.215.222.186:8005/Api/QiNiu/GetAccess'; //获取下载文件（云存储）的token的url，url+七牛文件url
+			mod.QNGETTOKENDELETE = 'http://114.215.222.186:8005/Api/QiNiu/Delete'; //获取批量（或者一个）删除七牛文件的token的url
+			//---七牛空间和接口---测试---end---
+			break;
+		default:
+			break;
+	}
+
+	mod.PERSONALINFO = 'personalInfo1111'; //个人信息，登录成功后返回值
+	mod.SHAKEHAND = 'ShakeHand'; //公钥，登录时，返回的握手信息，
+	mod.AUTOLOGIN = 'autoLogin'; //登录信息
+	mod.DOCUMENTSPATH = 'DOCUMENTSPATH'; //记录document的地址
+	mod.WAITING = '加载中...'; //
+	mod.UPLOADING = '上传中...';
+	mod.SIGNKEY = 'jsy309'; //签名密钥
+
+	return mod;
+
+})(storageKeyName || {});
