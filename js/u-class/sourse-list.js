5b8925715dc5dc9f502aad7ac17d0322914f01ec
songyanming <1062608083@qq.com>
2017-08-30 14:10:02 +0800

    更新

--- js/u-class/sourse-list.js
+++ js/u-class/sourse-list.js
@@ -12,11 +12,11 @@ Vue.component("source-list", {
 		'<li class="mui-table-view-cell mui-media" v-for="item in listData">' +
 		'<a href="javascript:;">' +
 		'<img class="mui-media-object mui-pull-left" style="width: 40px;height: 40px;" src="../../images/utils/img-teacher-bg2.png">' +
-		'<div class="mui-media-body">' +
+		'<div class="mui-media-body"  style="white-space: pre;font-size:16px;color:#353535">' +
 		'{{item.name}}' +
 		'<p style="font-size: 12px;">{{item.create_time}}</p>' +
 		'</div>' +
-		'<button :id="item.id" style="margin-top: -10px;"  @click="download(item)" type="button" class="mui-btn mui-btn-warning mui-btn-outlined mui-pull-right">下载</button>' +
+		'<button :id="item.id" style="margin-top: -10px;background-color:#edf9ff;color:#37b9fe;font-size:15px;border-color:#37b9fe"  @click="download(item)" type="button" class="mui-btn mui-btn-warning mui-btn-outlined mui-pull-right">下载</button>' +
 		'</a>' +
 		'</li>' +
 		'</ul>',
